11c697521cebd6ee53ea1ce7413145a880abd3d0
Safaa brouk <63406961+safaabrouk@users.noreply.github.com>
2024-06-22 02:41:59 +0100

    Add tools

---
+++ Site Web/src/tools/Role.java
@@ -0,0 +1,7 @@
+package tools;
+
+public enum Role {
+
+	utilisateur , bibliothecaire , admin
+	
+}
---
+++ Site Web/src/tools/SendMail.java
@@ -0,0 +1,59 @@
+package tools;
+
+import java.util.Properties;
+
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class SendMail {
+
+	public static void sendCode(String login, String code) {
+		
+		final String username = "biblio.readmore2020@gmail.com";
+		final String password = "mimisofisafaa1999";
+      Properties props = new Properties();
+      props.put("mail.smtp.host", "smtp.gmail.com");    
+      props.put("mail.smtp.port", "465");    
+      props.put("mail.smtp.auth", "true");    
+      props.put("mail.smtp.socketFactory.port", "465");    
+      props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");     
+      
+      Session session = Session.getInstance(props,
+    		  new javax.mail.Authenticator(){
+    	   protected PasswordAuthentication getPasswordAuthentication() {
+    		   return new PasswordAuthentication(username,password);
+    	   }
+      });
+   
+     try {
+    	 
+    	 Message message = new MimeMessage(session);
+    	 message.setFrom(new InternetAddress("from@gmail.com"));
+    	 message.setRecipients(
+    			 Message.RecipientType.TO,
+    			 InternetAddress.parse(login)
+    			 );
+    	 message.setSubject("Recuperer votre mot de passe ");
+    	 message.setText("Nous avons reçu une demande de réinitialisation de votre mot de passe \n" + 
+    	 		"Entrez le code de réinitialisation du mot de passe suivant : \n"+code);
+    	  Transport.send(message);
+		
+	} catch (MessagingException e) {
+		
+		e.printStackTrace();
+		
+	}
+     
+     
+	}
+	
+}
+
+
+
